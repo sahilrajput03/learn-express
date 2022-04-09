1a1bd3a7c3f0eb9e43b5b1af9d6a58d892fb5daf
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-09 14:28:32 +0530

    up

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/crash', (req, res) => {
+
+  // Using throw new Error() is really good. The significant benefit from `throw "my message"` is that with `new Error()` we get the stacktrace which helps to track the error at the exact line number in the program.
+  // 1. Send response (with status 500) to request at `/crash` and
+  // 2. log to terminal at the same time!! Yo! This is so convenient and
+  // 3. This will not crash the server at all coz express catches the exception internally.
+	throw new Error('>>>Intentionally crash server to test livenessProbe, i.e., this container/pod will be restarted when noticed by livenessProbe anytime soon.')
+  
+
+  //  code here won't be executed ..
+}
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
