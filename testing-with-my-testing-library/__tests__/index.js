cedbb6f60ebad1e97dd924162df002feda44e090
Sahil Rajput <sahilrajput03@gmail.com>
2022-05-08 01:18:43 +0530

    Up

--- testing-with-my-testing-library/__tests__/index.js
+++ testing-with-my-testing-library/__tests__/index.js
@@ -1,4 +1,5 @@
-require('./_setup_test_globals.js')
+// Setup your test globals
+require('flash')
 
 // Define your test files here...(or you can fetch all .test.js files manually in this directory as well with nodejs api.
 require('./withSupertest.test.js')
