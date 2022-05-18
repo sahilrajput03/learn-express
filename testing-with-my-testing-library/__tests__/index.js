c8fc807d82779b9a6f6013d2c64d40e2001a5256
Sahil Rajput <sahilrajput03@gmail.com>
2022-05-18 09:21:16 +0530

    chore:fix

--- testing-with-my-testing-library/__tests__/index.js
+++ testing-with-my-testing-library/__tests__/index.js
@@ -1,5 +1,5 @@
 // Setup your test globals
-require('flash')
+require('flash-runner/_setup_test_globals.js')
 
 // Define your test files here...(or you can fetch all .test.js files manually in this directory as well with nodejs api.
 require('./withSupertest.test.js')
