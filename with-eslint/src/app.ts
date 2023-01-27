2c6de24ca1508389fef1ebb3e5b89880204c222c
Sahil Rajput <sahilrajput03@gmail.com>
2023-01-27 21:46:15 +0530

    Add with eslint typescript config.

--- with-eslint/src/app.ts
+++ with-eslint/src/app.ts
@@ -1,13 +1,14 @@
-import express, { Application, Request, Response } from 'express'
+import express from 'express'
+import type { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
-const port: number = 3001
+const port = 3001
 
 app.get('/toto', (req: Request, res: Response) => {
-    res.send('Hello toto')
+  res.send('Hello toto')
 })
 
 app.listen(port, function () {
-    console.log(`App is listening on port ${port} !`)
+  console.log(`App is listening on port ${port} !`)
 })
