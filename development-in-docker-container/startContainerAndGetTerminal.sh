1c297d95af61014b6eec8144e5051466715025d9
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-15 22:52:26 +0530

    Update startContainerAndGetTerminal.sh

--- development-in-docker-container/startContainerAndGetTerminal.sh
+++ development-in-docker-container/startContainerAndGetTerminal.sh
@@ -4,12 +4,11 @@ shopt -s expand_aliases
 
 # LEARN: The container would be stopped as soon as you exit the container's shell with below command as it is.
 
-# LEARN: The reason I am using this as variable coz it would be much conveninent to copy this file to some other project and run this file to start the development mode in seconds without additional configuration needed (FYI: Do use correct image with appropriate node version which suits your application needs though). Yo!
-app_path=mydir
+APP_PATH=mydir
 IMAGE=node:12
 
 # LEARN : You may choose to mount any directory instead of /mydir, i.e., /somedir and somedir will be automicatically created when the container is started. YO!
-dk run -it -v $(pwd):/$app_path -p 8086:8086 -p 9229:9229 $IMAGE sh -c "cd /$app_path; bash"
+dk run -it -v $(pwd):/$APP_PATH -p 8086:8086 -p 9229:9229 $IMAGE sh -c "cd /$APP_PATH; bash"
 
 #LEARN: Port 9229 is for sharing the debugging port. Port 9229 is used default by nodejs, but we can change this by passing debugPortNumber via `nodemon` or `node` cli parameters easily and configure vscode(changing port number in `launch.json` configuration in `port` property) to work with that as well very easily.
 
