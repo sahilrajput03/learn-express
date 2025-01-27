ccef950d8515ea71f8bd6985c69e4539c17e12aa
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-27 18:36:32 +0530

    Update.

--- learn-node/fork/child.js
+++ learn-node/fork/child.js
@@ -1,4 +1,7 @@
 process.on('message', (message) => {
     console.log('|CHILD|', 'Received from parent:', message);
     process.send({ status: 'Success' });
+    console.log('child.js PROCESS ID:', process.pid);
 });
+
+// setInterval(() => { }, 10000_000); // mimic eternally running server
--- learn-node/fork/parent.js
+++ learn-node/fork/parent.js
@@ -6,3 +6,18 @@ child.send({ value: 42 });
 child.on('message', (message) => {
     console.log('|PARENT|', 'Received from child:', message);
 });
+
+// Kill child processes (to prevent orphan processes) in case of normal exit via `process.exit()` of this process *OR* run time exception of this process.
+process.on('exit', () => {
+    console.log('Parent is exiting. Terminating child process...');
+    child.kill(); // Ensure the child process is killed
+});
+
+// * Testing killing of child in case of normal exit or run time exception when child is eternally running script
+// setTimeout(() => {
+//     process.exit(0);
+// }, 10_000);
+
+// setTimeout(() => {
+//     throw new Error('heyyyy');
+// }, 3_000);
