839b7b38956f62724fac3b5c64b0d2df5ec57cd5
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-29 21:34:14 +0530

    Update.

--- learn-node/kill-switch-process/main.js
+++ learn-node/kill-switch-process/main.js
@@ -4,8 +4,8 @@ const command = 'node spm.js';
 const child = spawn('sh', ['-c', command], {
     cwd: __dirname,
     detached: true, // Run independently
-    stdio: "ignore", // Ignore parent’s stdio
-    // stdio: "inherit", // Ignore parent’s stdio (// ! Enable this for debugging for e.g, when there is error in json syntax)
+    stdio: "ignore", // Ignore child’s stdio
+    // stdio: "inherit", // Ignore child’s stdio (// ! Enable this for debugging for e.g, when there is error in json syntax)
 });
 child.unref(); // Ensure the child is fully detached
 
