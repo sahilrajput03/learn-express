e23c603862e119c21209227a5743c76e31063695
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-30 20:14:38 +0530

    Update.

--- learn-node/kill-switch-process/spm-initializer.js
+++ learn-node/kill-switch-process/spm-initializer.js
@@ -1,20 +1,37 @@
 const { spawn } = require('child_process');
+const fs = require('fs');
+const { kill } = require('process');
+const { checkIfProcessIsRunning } = require('./spm-utils');
 
 const spmInitializer = () => {
-    const command = 'node spm.js';
-    const child = spawn('sh', ['-c', command], {
-        cwd: __dirname,
-        detached: true, // Run independently in detached state
-        // * Note: You must use below `stdio: "ignore"` otherwise your detached child will be killed by playwright cli when the test are finished (irrespective if you use `nodemon` or not). Also if you are directly running `node spm-initializer.js` for standalone testing the `spm.js` will stop working as soon as you close/kill the terminal (`exit` command).
-        stdio: "ignore", // Ignore child’s stdio
-        //  Enable below limne for debugging for debugging only for e.g., when there is error in json syntax in the `spm.json` file, checking logs of spm.js file, etc.
-        // stdio: "inherit", // Inherit child’s stdio (
-    });
-    child.unref(); // Ensure the child is fully detached
+    // if both spmJs and its child are running then do not do anything.
+    const jsonData = JSON.parse(fs.readFileSync('spm.json', 'utf8'));
 
-    // This `main.js` process is terminated asap & you can verify via
-    // `ps aux | grep [1]234`   (where 1234 is the below process id)
-    console.log('process id of main.js', process.pid);
+    // console.log(checkIfProcessIsRunning(jsonData.pid.spmJs), checkIfProcessIsRunning(jsonData.pid.child));
+    if (checkIfProcessIsRunning(jsonData.pid.spmJs) && checkIfProcessIsRunning(jsonData.pid.child)) {
+        console.log('Error: Cannot spawn prcesses because both processes are running well already!');
+        return; // Stop the program here.
+    } else {
+        // This runs when only one process (of the two process ids) mentioned in spm.json file is running,
+        // so we do a cleanup by killing that one process so that we start only fresh processes only
+        if (checkIfProcessIsRunning(jsonData.pid.spmJs)) { kill(jsonData.pid.spmJs); }
+        if (checkIfProcessIsRunning(jsonData.pid.child)) { kill(jsonData.pid.child); }
+
+        const commandSpm = 'node spm.js';
+        const child = spawn('sh', ['-c', commandSpm], {
+            cwd: __dirname,
+            detached: true, // Run independently in detached state
+            // * Note: You must use below `stdio: "ignore"` otherwise your detached child will be killed by playwright cli when the test are finished (irrespective if you use `nodemon` or not). Also if you are directly running `node spm-initializer.js` for standalone testing the `spm.js` will stop working as soon as you close/kill the terminal (`exit` command).
+            stdio: "ignore", // Ignore child’s stdio
+            //  Enable below limne for debugging for debugging only for e.g., when there is error in json syntax in the `spm.json` file, checking logs of spm.js file, etc.
+            // stdio: "inherit", // Inherit child’s stdio (
+        });
+        child.unref(); // Ensure the child is fully detached
+
+        // This `spm-initializer.js` process is terminated asap & you can verify via
+        // `ps aux | grep [1]234`   (where 1234 is the below process id)
+        console.log('process id of spm-initializer.js', process.pid);
+    }
 };
 
 // I use below line test spm  and // * if you want to use spm in some other project please simply copy these 3 files then export the above `spmInitializer` function and call it where you need it to run.
--- learn-node/kill-switch-process/spm.js
+++ learn-node/kill-switch-process/spm.js
@@ -2,10 +2,11 @@
 const { spawn } = require('child_process');
 const { log } = require('console');
 const fs = require('fs');
+const { getHumanReadableTimestamp } = require('./spm-utils');
 
-const backgroundProcessCommand = 'npx tsc --watch'; // Also: build doesn't exists --- a fresh build is created automatically.
+const command = 'npx tsc --watch'; // Also: build doesn't exists --- a fresh build is created automatically.
 const cwd = '/Users/apple/Documents/test/qr-solution/qr-solution-backend';
-const child = spawn('sh', ['-c', backgroundProcessCommand], {
+const child = spawn('sh', ['-c', command], {
     cwd,
 });
 const filePath = 'spm.json';
@@ -21,19 +22,17 @@ const syncSwitchProcessJsonFile = () => {
         if (shouldExit) {
             log('should kill noww!!');
             jsonData.killNow = 0;
-            jsonData.isRunning = false;
-            jsonData.pid = null;
+            jsonData.pid = { spmJs: null, child: null };
             jsonData.lastCheckTime = null;
         } else {
-            jsonData.isRunning = true;
-            jsonData.pid = child.pid;
+            jsonData.pid = { spmJs: process.pid, child: child.pid };
             jsonData.lastCheckTime = getHumanReadableTimestamp();
         }
 
         fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), 'utf8');
         if (shouldExit) {
-            child.kill();
-            process.exit(0);
+            child.kill(); // npx tsc --watch
+            process.exit(0); // spm.js
         }
     } catch (error) { console.error('Error reading file:', error); }
 };
@@ -42,21 +41,3 @@ const syncSwitchProcessJsonFile = () => {
 syncSwitchProcessJsonFile();
 // poll = 3 seconds
 setInterval(syncSwitchProcessJsonFile, 3_000);
-
-
-
-// 17:40:43 - 29 January 2025
-function getHumanReadableTimestamp() {
-    const now = new Date();
-
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-
-    const day = now.getDate();
-    const month = now.toLocaleString('default', { month: 'long' });
-    const year = now.getFullYear();
-
-    // Sample output: "17:40:43 - 29 January 2025"
-    return `${hours}:${minutes}:${seconds} - ${day} ${month} ${year}`;
-};
