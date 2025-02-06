aa215159786e1b74357382d3a06dcfee071954a7
Sahil Rajput <sahilrajput03@gmail.com>
2025-02-07 05:13:00 +0530

    Update.

--- learn-multer/backend/controllers/product.ts
+++ learn-multer/backend/controllers/product.ts
@@ -1,5 +1,23 @@
 import fs from 'fs'
+import { upload } from '../middlewares/multer';
+import multer from 'multer';
 
+// Docs: To catch errors specifically from Multer, you can call the middleware function by yourself: (src - https://www.npmjs.com/package/multer#error-handling)
+export const newUserUploadMiddleware = async (req: any, res: any, next: any) => {
+  const fieldName = "photo"
+  upload.single(fieldName)(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log('A Multer error occurred when uploading.') // [E.g., MulterError: Unexpected field]
+      if (err.name === "MulterError" && err.message === 'Unexpected field') {
+        // handle here...
+        res.status(400).json({ success: false, message: `Please make sure you are uploading to \`${fieldName}\` fieldName to upload a single file only.` });
+      }
+    } else if (err) {
+      console.log('An unknown error occurred when uploading.')
+    }
+    next(err) // Everything went fine.
+  })
+}
 export const newUser = async (req: any, res: any, next: any) => {
   const photo = req.file as Express.Multer.File | undefined;
   console.log("🚀 photo:", photo)
@@ -28,6 +46,23 @@ export const newUser = async (req: any, res: any, next: any) => {
   return
 };
 
+// Docs: To catch errors specifically from Multer, you can call the middleware function by yourself: (src - https://www.npmjs.com/package/multer#error-handling)
+export const newProductUploadMiddleware = async (req: any, res: any, next: any) => {
+  const fieldName = "photos"
+  const maxAllowed = 2
+  upload.array(fieldName, maxAllowed)(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log('A Multer error occurred when uploading.') // [E.g., MulterError: Unexpected field]
+      if (err.name === "MulterError" && err.message === 'Unexpected field') {
+        // handle here...
+        res.status(400).json({ success: false, message: `Please make sure you are uploading to \`${fieldName}\` field and not more than ${maxAllowed} files.` });
+      }
+    } else if (err) {
+      console.log('An unknown error occurred when uploading.')
+    }
+    next(err) // Everything went fine.
+  })
+}
 export const newProduct = async (req: any, res: any, next: any) => {
   const photos = req.files as Express.Multer.File[] | undefined;
   console.log("🚀 photos:", photos)
--- learn-multer/backend/src/app.ts
+++ learn-multer/backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import type { Application, Request, Response } from 'express'
-import { newProduct, newUser } from '../controllers/product'
-import { upload } from '../middlewares/multer'
+import { newProduct, newProductUploadMiddleware, newUser, newUserUploadMiddleware } from '../controllers/product'
 
 const app: Application = express()
 
@@ -11,8 +10,8 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello from express + typescript')
 })
 
-app.put("/new-user", upload.single("photo"), newUser)
-app.put("/new-product", upload.array("photos", 2 /** max allowed */), newProduct)
+app.put("/new-user", newUserUploadMiddleware, newUser)
+app.put("/new-product", newProductUploadMiddleware, newProduct)
 
 app.listen(port, function () {
   console.log(`App running on: http://localhost:${port}`)
