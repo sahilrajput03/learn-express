30721db23f8656a8ad46ac3419ae68621d096ce9
Sahil Rajput <sahilrajput03@gmail.com>
2025-02-10 23:35:35 +0530

    Update.

--- learn-multer/frontend/playwright.config.ts
+++ learn-multer/frontend/playwright.config.ts
@@ -71,10 +71,12 @@ export default defineConfig({
     // },
   ],
 
+  // Note to Sahil: I enabled below commented code as it helps to reuse the
+  // existing dev server if running otherwise start the dev server [tested].
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'nps dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
 });
