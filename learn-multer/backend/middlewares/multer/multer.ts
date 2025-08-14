f7374cf872edc775c14b560afc0ac35931eb078a
Sahil Rajput <sahilrajput03@gmail.com>
2025-08-15 02:55:12 +0530

    Update.

--- learn-multer/backend/middlewares/multer/multer.ts
+++ learn-multer/backend/middlewares/multer/multer.ts
@@ -1,5 +1,6 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
+import * as mime from 'mime-types';
 
 // We need to configure `storage` so that files are stored along with their extensions.
 const storage = multer.diskStorage({
@@ -8,7 +9,15 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const id = uuid();
-        const extName = file.originalname.split(".").pop();
+        let extName = file.originalname.split(".").pop();
+        // In tests, when I upload a pdf file I'm facing the issue that any pdf
+        //      file is uploaded with name e.g, `xxx....xxx.blob` (ending with
+        //      .blob) in the `local-storage-for-tests` folder, and to fix this
+        //      issue I'm using `mime` package to get proper file extension 
+        //      ('pdf') from the mimetype ('application/pdf').
+        if (extName === 'blob') {
+            extName = mime.extension(file.mimetype) // 'application/pdf' -> 'pdf'
+        }
         const fileName = `${id}.${extName}`
         cb(null, fileName);
     }
