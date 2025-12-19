0bddc6675bc034531df0e88c33dfefd53d2728f1
Sahil Rajput <sahilrajput03@gmail.com>
2025-12-19 17:33:43 +0530

    Update.

--- node-push-notifications/client/worker.js
+++ node-push-notifications/client/worker.js
@@ -3,7 +3,7 @@ console.log("Service Worker Loaded...");
 
 self.addEventListener("push", e => {
   const data = e.data.json();
-  console.log("Push Recieved...");
+  console.log("Push Received...");
   self.registration.showNotification(data.title, {
     body: "Notified by Traversy Media!",
     icon: "http://image.ibb.co/frYOFd/tmlogo.png"
