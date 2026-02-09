96b5ae8463be23748c92dc1a61ef1b6cb8a641a4
Sahil Rajput <sahilrajput03@gmail.com>
2026-02-09 23:12:27 +0530

    Update.

--- learn-node/read-input-from-stdin/with-readline.js
+++ learn-node/read-input-from-stdin/with-readline.js
@@ -2,6 +2,9 @@ const readline = require("node:readline/promises");
 
 // @ts-ignore
 async function ask(questionText) {
+    // Learn: Why sleep? Because without it if I put a console log before asking any question then the question appears on same line as of the previous log.
+    await new Promise(res => setTimeout(res, 20)); // This sleep is necessary 
+
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
